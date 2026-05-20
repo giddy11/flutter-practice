8bf22c0b3f238c3fcc479ee99b53409dbc91d998
giddy <edoghotugiddy@gmail.com>
2026-05-21 00:08:57 +0100

    Update drink.dart

--- restaurantordering_system/drink.dart
+++ restaurantordering_system/drink.dart
@@ -6,4 +6,14 @@ class Drink extends MenuItem {
   bool isCold;
 
   Drink(String name, double price, FoodCategory category, this.size, this.isCold) : super(name, price, category);
+
+  void displayItem() {
+    super.displayItem();
+    print("Size: ${size}.");
+    if(isCold){
+      print("The drink temperature is cold");
+    } else{
+      print("The drink temperature is hot");
+    }
+  }
 }
